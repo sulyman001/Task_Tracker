452288660db2585b68dff47651929ee97ab71e81
Sulyman <sulymansulyman001@gmail.com>
2023-01-30 19:55:35 +0100

    Created a new task tracker

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,45 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Task } from 'src/app/task';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs'
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css']
+})
+export class AddTaskComponent {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+  text!: string;
+  day!: string;
+  reminder: boolean = false;
+  showAddTask: boolean = false;
+  subscription: Subscription;
+
+
+  constructor(private  uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value));
+  }
+
+  onSubmit() {
+    if(!this.text){
+      alert('Please add a task!');
+      return;
+    } else if(!this.day){
+      alert('Please add a date!');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
+}
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs'
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  title: string = 'Task Tracker';
+  showAddTask: boolean = false;
+  subscription: Subscription = new Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value));
+  }
+
+  ngOnInit(): void {}
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
+}
